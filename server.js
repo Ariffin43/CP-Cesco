1dc5b0bd4b906f271fd495ce0c95865d5a951fd3
Ariffin43 <bustanulariffin05@gmail.com>
2025-09-16 11:17:51 +0700

    update sekali lagi

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const { createServer } = require("http");
-const next = require("next");
+const { createServer } = require('http');
+const next = require('next');
 
-const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(port, err => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log('🚀 Server ready on port 3000');
   });
 });

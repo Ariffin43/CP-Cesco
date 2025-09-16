9d5d87c1d28e2fdd6e35ae23ca8c427da1f2ecce
Ariffin43 <bustanulariffin05@gmail.com>
2025-09-16 09:46:40 +0700

    update lgi

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { createServer } = require("http");
 const next = require("next");
-const { parse } = require("url");
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -8,10 +7,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
+  createServer((req, res) => handle(req, res)).listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });

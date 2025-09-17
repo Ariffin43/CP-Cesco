75d2f17c1e9d07afb53c3fce2ef420d8c4560dbf
Ariffin43 <bustanulariffin05@gmail.com>
2025-09-17 09:12:02 +0700

    tes deploy sekian kalinya

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = process.env.port || 3000
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
       if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
+        await app.render(req, res, '/a', query);
       } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
+        await app.render(req, res, '/b', query);
       } else {
-        await handle(req, res, parsedUrl)
+        await handle(req, res, parsedUrl);
       }
     } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
     }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
-  })
-})
+  }).listen(port, '0.0.0.0', () => {
+    console.log(`> Ready on 0.0.0.0:${port}`);
+  });
+});
